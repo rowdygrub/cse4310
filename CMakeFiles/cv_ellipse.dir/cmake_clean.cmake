36209049caf283a82f23058f0d454c1e2aa85a12
henry <henrybang09@gmail.com>
2018-07-06 12:05:55 -0500

    First commit, need to fix multiple ellipse drawing

---
+++ CMakeFiles/cv_ellipse.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_ellipse.dir/cv_ellipse.cpp.o"
+  "cv_ellipse.pdb"
+  "cv_ellipse"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_ellipse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/cv_ransac.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_ransac.dir/cv_ransac.cpp.o"
+  "cv_ransac.pdb"
+  "cv_ransac"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_ransac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
